a2149352d2a12d8fdd94557e12ccb1991b00fb56
Павел <pasha.seregin.92@mail.ru>
2022-06-08 17:09:16 +0300

    Исправления функций

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-const { Linter } = require("eslint");
-
 function getRandomInteger(min, max) {
   if (min < 0 || max < 0) {
-    console.log("Вводимые значения не должны быть отрицательными");
+    // Вводимые значения не должны быть отрицательными
   } else if (max <= min) {
-    console.log("Значение 'до' не может быть меньше либо рано значению 'от'");
+    // Значение 'до' не может быть меньше либо рано значению 'от'
   } else {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
 
-const maxLine = 140;
+getRandomInteger();
 
-function checkingMaxLenght(lenght, maxLine) {
-  if (lenght.maxLine > maxLine) {
+function checkingMaxLenght(str, maxLenght) {
+  if (str.Lenght > maxLenght) {
     return false;
   } else {
     return true;
   }
 }
+
+checkingMaxLenght();
