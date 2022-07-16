21395bf92eefd1eb4ff54c9dc870e186fbec4a31
Павел <pasha.seregin.92@mail.ru>
2022-07-16 14:10:56 +0300

    Исправления3

--- js/form.js
+++ js/form.js
@@ -14,18 +14,20 @@ const onFormEscKeydown = (evt) => {
     imgUploadOverlay.classList.add('hidden');
     body.classList.remove('modal-open');
     document.removeEventListener('keydown', onFormEscKeydown);
+    uploadFile.value = '';
   }
 };
 
-function offFormClick() {
+function setFormCloseHandler() {
   uploadCancel.addEventListener('click', () => {
     imgUploadOverlay.classList.add('hidden');
     body.classList.remove('modal-open');
     document.removeEventListener('keydown', onFormEscKeydown);
+    uploadFile.value = '';
   });
 }
 
-function onFormChange() {
+function setFormChangeHandler() {
   uploadFile.addEventListener('change', () => {
     imgUploadOverlay.classList.remove('hidden');
     body.classList.add('modal-open');
@@ -41,24 +43,30 @@ function onHashTagInputValid() {
     hashtagInput.setCustomValidity(
       `Хэш-тегов не должно быть больше чем ${MAX_HASHTAG_NUMBERS}`
     );
+    hashtagInput.reportValidity();
     return;
   } else {
     hashtagInput.setCustomValidity('');
   }
 
-  hashTagArray.forEach((hashtag) => {
-    if (!HASHTAG_VALID_REGEX.test(hashtag)) {
+  if (hashTagArray.length !== uniqueHashTagArray.size) {
+    hashtagInput.setCustomValidity('Хэш-теги не должны повторяться');
+    hashtagInput.reportValidity();
+    return;
+  } else {
+    hashtagInput.setCustomValidity('');
+  }
+
+  for (let i = 0; i < hashTagArray.length; ++i) {
+    if (!HASHTAG_VALID_REGEX.test(i)) {
       hashtagInput.setCustomValidity(
         `Хэш-тег должен начинается с символа # (решётка)
 
         Хэш-тег должен состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.`
       );
-    } else if (hashTagArray.length !== uniqueHashTagArray.size) {
-      hashtagInput.setCustomValidity('Хэш-теги не должны повторяться');
-    } else {
-      hashtagInput.setCustomValidity('');
+      break;
     }
-  });
+  }
 
   hashtagInput.reportValidity();
 }
@@ -77,4 +85,4 @@ commentInput.addEventListener('keydown', (evt) => {
   }
 });
 
-export { onFormChange, offFormClick };
+export { setFormChangeHandler, setFormCloseHandler };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import './utils.js';
 import './data.js';
 import { renderPhotos } from './picture.js';
-import { onFormChange, offFormClick } from './form.js';
+import { setFormChangeHandler, setFormCloseHandler } from './form.js';
 import { closeBigPicture } from './big-picture.js';
 
 renderPhotos();
 closeBigPicture();
-onFormChange();
-offFormClick();
+setFormChangeHandler();
+setFormCloseHandler();
